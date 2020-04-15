92f8004dab9f44bcb48c212c894de981170dc629
k.yamada <yetanother.yk@gmail.com>
2020-04-16 00:18:22 +0900

    リファクタリング

--- components/SubwayStatusBoard.vue
+++ components/SubwayStatusBoard.vue
@@ -14,31 +14,6 @@ import Vue from 'vue'
 export default Vue.extend({
   components: {
     // SubwayStatus,
-  },
-  data() {
-    return {
-      // intervalId: undefined,    // 1. clearIntervalのためのIDを保持します
-      // time: undefined,
-      intervalId: 0, // 1. clearIntervalのためのIDを保持します
-      time: new Date()
-    }
-  },
-  mounted() {
-    // 3. 上記で用意したメソッドをマウントのタイミングで呼び出します。
-    //    これによりこのコンポーネントは一定間隔で処理を実行することになります。
-    this.do()
-  },
-  beforeDestroy() {
-    // 4. 使用後はしっかりとクリアする必要があります
-    clearInterval(this.intervalId)
-  },
-  methods: {
-    do() {
-      // 2. 一定間隔で処理を実行するためメソッドを用意します
-      this.intervalId = window.setInterval(() => {
-        this.time = new Date() // 処理内容
-      }, 1000) // 1秒間隔で処理
-    }
   }
 })
 </script>
