4a6c4dbc344f7475b1698416d4a53b11f801186e
k.yamada <yetanother.yk@gmail.com>
2020-04-16 00:17:07 +0900

    取得の中止

--- store/TraficStatus.js
+++ store/TraficStatus.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+// import axios from 'axios'
 
 export const state = () => ({
-  data: {}
+  data: '将来的に地下鉄の運行情報を出す'
 })
 
 export const mutations = {
@@ -10,15 +10,15 @@ export const mutations = {
   }
 }
 export const actions = {
-  async getNowStatus(context) {
-    await axios
-      .get(
-        `https://ckan.pf-sapporo.jp/api/action/datastore_search_sql?sql=SELECT%20*%20from%20%22${process.env.subwayStatusResourceId}%22%20ORDER%20BY%20date%20desc,%20time%20desc%20LIMIT%201`
-      )
-      .then((res) => {
-        if (res.data.success) {
-          context.commit('setData', res.data.result.records[0])
-        }
-      })
+  async getNowStatus(_context) {
+    // await axios
+    //   .get(
+    //     `https://ckan.pf-sapporo.jp/api/action/datastore_search_sql?sql=SELECT%20*%20from%20%22${process.env.subwayStatusResourceId}%22%20ORDER%20BY%20date%20desc,%20time%20desc%20LIMIT%201`
+    //   )
+    //   .then((res) => {
+    //     if (res.data.success) {
+    //       context.commit('setData', res.data.result.records[0])
+    //     }
+    //   })
   }
 }
