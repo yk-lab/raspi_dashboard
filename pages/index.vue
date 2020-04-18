3245b5e34fe029e80052a0b6428836bd9a0d8355
k.yamada <yetanother.yk@gmail.com>
2020-04-19 00:53:03 +0900

    ニュースの表示・TODO更新

--- pages/index.vue
+++ pages/index.vue
@@ -3,25 +3,32 @@
     v-col.pa-0(cols="6")
       Clock.ma-2
       Weather.ma-2
-    v-col(cols="6")
-      SubwayStatusBoard
-      EarthquakeSnackbar
-      div ニュース
-      div 渋滞
+    v-col.pa-0(cols="6")
+      TopNews.ma-2
+      LocalNews.ma-2
+      SubwayStatusBoard.ma-2
+      div.ma-2 道路情報
+      div.ma-2 ニュース速報
+      div.ma-2 気象庁 警報・注意報
+    EarthquakeSnackbar
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Clock from '~/components/Clock.vue'
 import EarthquakeSnackbar from '~/components/EarthquakeSnackbar.vue'
+import LocalNews from '~/components/LocalNews.vue'
 import SubwayStatusBoard from '~/components/SubwayStatusBoard.vue'
+import TopNews from '~/components/TopNews.vue'
 import Weather from '~/components/Weather.vue'
 
 export default Vue.extend({
   components: {
     Clock,
     EarthquakeSnackbar,
+    LocalNews,
     SubwayStatusBoard,
+    TopNews,
     Weather
   },
   data() {
